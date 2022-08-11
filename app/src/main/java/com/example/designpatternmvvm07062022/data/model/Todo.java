0192ef18857acfad422bb2de47e6549a031c6028
Phat <phatdroid94@gmail.com>
2022-08-11 20:27:04 +0700

    Initial commit

---
+++ app/src/main/java/com/example/designpatternmvvm07062022/data/model/Todo.java
@@ -0,0 +1,38 @@
+package com.example.designpatternmvvm07062022.data.model;
+
+/**
+ * Created by pphat on 8/11/2022.
+ */
+public class Todo {
+    private String title;
+    private String description;
+
+    public Todo(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Todo{" +
+                "title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
