85628896edbcefc1efcb8d13ce852dab17e6d270
Phat <phatdroid94@gmail.com>
2022-08-13 20:04:59 +0700

    data mock

--- app/src/main/java/com/example/designpatternmvvm07062022/presentation/activities/MainActivity.java
+++ app/src/main/java/com/example/designpatternmvvm07062022/presentation/activities/MainActivity.java
@@ -6,18 +6,26 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 
 import com.example.designpatternmvvm07062022.R;
+import com.example.designpatternmvvm07062022.data.model.Todo;
 import com.example.designpatternmvvm07062022.presentation.adapters.TodoAdapter;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
     RecyclerView rcvTodo;
     TodoAdapter todoAdapter;
-
+    List<Todo> todoList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         rcvTodo = findViewById(R.id.recyclerview_todo);
+        todoList = Todo.getMock();
         todoAdapter = new TodoAdapter();
+        todoAdapter.setTodoList(todoList);
+        rcvTodo.setAdapter(todoAdapter);
+        rcvTodo.setHasFixedSize(true);
     }
 }
